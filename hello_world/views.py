44139a992ca67fa93abc54555aa5bc050d728ef6
Mohit Gupta <mohitgupta@Mohits-MacBook-Air.local>
2019-03-11 13:12:09 +0530

    hello world is now render

--- hello_world/views.py
+++ hello_world/views.py
@@ -2,7 +2,7 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http.response import HttpResponse
 
 # Create your views here.
 def homePageView(request):
